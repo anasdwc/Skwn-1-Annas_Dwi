cf2dbfb7562044559b1ecd8c1b2d8d75ba681b88
Annas Dwi <manasdwi276@gmail.com>
2023-01-19 09:18:56 +0700

    add desktop style

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,37 +1,39 @@
 export default function Footer() {
   return (
-    <footer className="flex flex-col items-center py-8 px-4 text-xs">
-      <div className="brand-logo">
-        <img src="/images/logo.png" alt="" />
+    <footer className="flex flex-col items-center py-8 px-4 text-xs md:flex-row md:gap-x-28 md:py-20 md:px-28">
+      <div className="md:text-[#667085}">
+        <div className="brand-logo flex justify-center md:justify-start">
+          <img src="/images/logo.png" alt="" />
+        </div>
+        <div className="brand-description my-9 text-[#667085]">
+          <p className="text-center leading-6 md:pr-32 md:text-left">
+            Dekoor is a furniture company created to fulfill the needs of family
+            with aesthetic feeling in their furniture. Always pay attention to
+            details and give clear communication for the customers. Priority of
+            our design is comfortability.
+          </p>
+        </div>
+        <div className="copyright order-3 hidden text-[#667085] md:block">
+          &copy; Copyright 2022 Dekoor
+        </div>
       </div>
-      <div className="brand-description my-9 text-[#303030]">
-        <p className="text-center leading-6">
-          Dekoor is a furniture company created to fulfill the needs of family
-          with aesthetic feeling in their furniture. Always pay attention to
-          details and give clear communication for the customers. Priority of
-          our design is comfortability.
-        </p>
-      </div>
-      <div className="copyright order-3 text-[#303030]">
-        &copy; Copyright 2022 Dekoor
-      </div>
-      <div className="footer-list mb-9 grid w-full grid-cols-2 gap-y-8 text-center text-xs">
-        <ul className="flex flex-col gap-6 text-[#303030]">
-          <li className="text-sm font-bold">Support</li>
+      <div className="footer-list grid w-full grid-cols-2 gap-y-8 text-center text-xs md:order-3 md:mb-0 md:grid-cols-3">
+        <ul className="flex flex-col gap-6 text-[#667085]">
+          <li className="text-sm font-bold text-[#101828]">Support</li>
           <li>FAQ</li>
           <li>Shipping & Returns</li>
           <li>Care guide</li>
           <li>Redeem warranty</li>
         </ul>
-        <ul className="flex flex-col gap-6 text-[#303030]">
-          <li className="text-sm font-bold">Social Media</li>
+        <ul className="flex flex-col gap-6 text-[#667085]">
+          <li className="text-sm font-bold text-[#101828]">Social Media</li>
           <li>Instagram</li>
           <li>Facebook</li>
           <li>Twitter</li>
           <li>TikTok</li>
         </ul>
-        <ul className="col-span-full flex flex-col gap-6 text-[#303030]">
-          <li className="text-sm font-bold">About Us</li>
+        <ul className="col-span-full flex flex-col gap-6 text-[#667085] md:col-start-3">
+          <li className="text-sm font-bold text-[#101828]">About Us</li>
           <li>Our Story</li>
           <li>Designer</li>
           <li>Craftmanship</li>
