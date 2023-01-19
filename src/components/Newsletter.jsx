55ad783c5f2d67d53949444ba141098f5bc42ec1
Annas Dwi <manasdwi276@gmail.com>
2023-01-19 09:09:16 +0700

    add desktop style

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -1,13 +1,13 @@
 export default function Newsletter() {
   return (
-    <section className="bg-[#FCFAFA] py-8 px-4 ">
-      <div className="px-8 text-center">
-        <h2 className="text-sm">LIMITED DEALS</h2>
-        <h3 className="mt-2 mb-6 font-bold">
+    <section className="bg-[#FCFAFA] py-8 px-4 md:px-32 md:py-20">
+      <div className="px-8 text-center text-very-black-brown md:text-left">
+        <h2 className="text-sm md:text-lg">LIMITED DEALS</h2>
+        <h3 className="mt-2 mb-6 font-bold md:my-10 md:text-6xl">
           Become a member and get 10% off of your first purchase
         </h3>
       </div>
-      <div className="relative px-8">
+      <div className="relative px-8 md:w-2/4">
         <input
           placeholder="Enter your email here"
           className="w-full rounded-lg p-4 placeholder:text-xs placeholder:text-black-brown"
